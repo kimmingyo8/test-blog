5519211e7d5862f09e8f7721f2eea5ffe1b4f50c
kimmingyo8 <kimmingyo8@naver.com>
2023-09-26 19:14:09 +0900

    게시글 없을때 처리

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -2,7 +2,7 @@ import AuthContext from 'context/AuthContext';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from 'firebaseApp';
 import { useContext, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 interface PostListProps {
   hasNavigation?: boolean;
@@ -23,6 +23,7 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
   const [activeTab, setActiveTab] = useState<TabType>('all');
   const [posts, setPosts] = useState<PostProps[]>([]);
   const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const getPosts = async () => {
     const data = await getDocs(collection(db, 'posts'));
@@ -59,30 +60,40 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
       )}
       <section className="post__list">
         <h1 className="a11y-hidden">게시글 목록</h1>
-        {posts?.length > 0
-          ? posts?.map((post, index) => (
-              <div key={post?.id} className="post__box">
-                <Link to={`/posts/${post?.id}`}>
-                  <ul className="post__profile-box">
-                    <li className="post__profile" />
-                    <li className="post__author-name">{post?.email}</li>
-                    <li className="post__date">{post?.createdAt}</li>
-                  </ul>
-                  <h2 className="post__title">{post?.title}</h2>
-                  <p className="post__text">{post?.content}</p>
-                </Link>
+        {posts?.length > 0 ? (
+          posts?.map((post, index) => (
+            <div key={post?.id} className="post__box">
+              <Link to={`/posts/${post?.id}`}>
+                <ul className="post__profile-box">
+                  <li className="post__profile" />
+                  <li className="post__author-name">{post?.email}</li>
+                  <li className="post__date">{post?.createdAt}</li>
+                </ul>
+                <h2 className="post__title">{post?.title}</h2>
+                <p className="post__text">{post?.content}</p>
+              </Link>
 
-                {post?.email === user?.email && (
-                  <ul className="post__utils-box">
-                    <li className="post__delete">삭제</li>
-                    <li className="post__edit">
-                      <Link to={`/posts/edit/${post?.id}`}>수정</Link>
-                    </li>
-                  </ul>
-                )}
-              </div>
-            ))
-          : '게시글이 없습니다.'}
+              {post?.email === user?.email && (
+                <ul className="post__utils-box">
+                  <li className="post__delete">삭제</li>
+                  <li className="post__edit">
+                    <Link to={`/posts/edit/${post?.id}`}>수정</Link>
+                  </li>
+                </ul>
+              )}
+            </div>
+          ))
+        ) : (
+          <h1 className="post__no-post">
+            <button
+              type="button"
+              className="post__btn-write"
+              onClick={() => navigate('/posts/new')}
+            >
+              게시글이 없습니다.
+            </button>
+          </h1>
+        )}
       </section>
     </>
   );
